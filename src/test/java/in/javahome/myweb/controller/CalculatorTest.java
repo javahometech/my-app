ff57e3e144790442f1344a0373f40c2002a94aac
javahometech <kammana.hari@gmail.com>
2018-11-01 11:12:58 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -11,4 +11,31 @@ public class CalculatorTest extends TestCase {
 	public void testMultiply(){
 		Assert.assertEquals(cal.multiply(10, 20), 200);
 	}
+	Properties prop = new Properties();
+		OutputStream output = null;
+
+		try {
+
+			output = new FileOutputStream("config.properties");
+
+			// set the properties value
+			prop.setProperty("database", "localhost");
+			prop.setProperty("dbuser", "hari");
+			prop.setProperty("dbpassword", "password");
+
+			// save properties to project root folder
+			prop.store(output, null);
+
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
 }
