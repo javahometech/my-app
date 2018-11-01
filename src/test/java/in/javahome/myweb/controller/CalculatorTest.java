c4a4ba22b21011570378604df52d0eab24106e5c
javahometech <kammana.hari@gmail.com>
2018-11-01 11:31:09 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -1,41 +1,53 @@
 package in.javahome.myweb.controller;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class CalculatorTest extends TestCase {
 	Calculator cal = new Calculator();
-	public void testAdd(){
+
+	public void testAdd() {
 		Assert.assertEquals(cal.add(10, 20), 30);
 	}
-	public void testMultiply(){
+
+	public void testMultiply() {
 		Assert.assertEquals(cal.multiply(10, 20), 200);
 	}
+
 	Properties prop = new Properties();
-		OutputStream output = null;
+	OutputStream output = null;
 
-		try {
+	try
+	{
 
-			output = new FileOutputStream("config.properties");
+		output = new FileOutputStream("config.properties");
 
-			// set the properties value
-			prop.setProperty("database", "localhost");
-			prop.setProperty("dbuser", "hari");
-			prop.setProperty("dbpassword", "password");
+		// set the properties value
+		prop.setProperty("database", "localhost");
+		prop.setProperty("dbuser", "hari");
+		prop.setProperty("dbpassword", "password");
 
-			// save properties to project root folder
-			prop.store(output, null);
+		// save properties to project root folder
+		prop.store(output, null);
 
-		} catch (IOException io) {
-			io.printStackTrace();
-		} finally {
-			if (output != null) {
-				try {
-					output.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+	}catch(
+	IOException io)
+	{
+		io.printStackTrace();
+	}finally
+	{
+		if (output != null) {
+			try {
+				output.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-
 		}
+
+	}
+}
 }
