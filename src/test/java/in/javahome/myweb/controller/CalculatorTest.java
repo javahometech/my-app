e16928fe700073775ee84b9c0a5f16372a1bb3b6
Ankit Singh <ankit.singh@globant.com>
2019-08-23 14:11:53 +0530

    Update CalculatorTest.java
    changes file

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -48,6 +48,6 @@ public class CalculatorTest extends TestCase {
 	}
 
 	public void testMultiply() {
-		Assert.assertEquals(cal.multiply(10, 20), 200);
+		Assert.assertEquals(cal.multiply(10, 20), 210);
 	}
 }
