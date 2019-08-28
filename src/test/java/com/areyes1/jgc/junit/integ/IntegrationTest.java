e3830bf69c8fbfc2780b0d6f13f2ea11383a559d
Ankit Singh <ankit.singh@globant.com>
2019-08-28 14:35:36 +0530

    updated

---
+++ src/test/java/com/areyes1/jgc/junit/integ/IntegrationTest.java
@@ -0,0 +1,5 @@
+package com.areyes1.jgc.junit.integ;
+
+public interface IntegrationTest {
+
+}
---
+++ src/test/java/com/areyes1/jgc/junit/integ/IntegrationTestSampleTest.java
@@ -0,0 +1,54 @@
+package com.areyes1.jgc.junit.integ;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.areyes1.jgc.junit.integ.JUnitAssertEqualsServiceExample;
+import com.areyes1.jgc.junit.integ.ServiceObject;
+
+import static org.junit.Assert.assertEquals;
+
+@Category(IntegrationTest.class)
+public class IntegrationTestSampleTest {
+
+private JUnitAssertEqualsServiceExample junitAssertEqualsServiceSample;
+private ServiceObject serviceObject;
+@Before
+public void setData() {
+serviceObject = new ServiceObject();
+junitAssertEqualsServiceSample = new JUnitAssertEqualsServiceExample();
+junitAssertEqualsServiceSample.initiateMetaData(serviceObject);
+}
+
+@Test
+public void testAssertEqualsFalse() {
+//	processed the item
+ServiceObject newServiceObject = new ServiceObject();
+junitAssertEqualsServiceSample.initiateMetaData(newServiceObject);
+junitAssertEqualsServiceSample.processObject(serviceObject);
+assertEquals(serviceObject,serviceObject);
+}
+
+@Test
+public void testAssertEquals() {
+junitAssertEqualsServiceSample.processObject(serviceObject);
+assertEquals(serviceObject,this.serviceObject);
+}
+
+@Test
+public void testAssertEqualsWithMessage() {
+junitAssertEqualsServiceSample.processObject(serviceObject);
+assertEquals(
+"Same Object",
+serviceObject,serviceObject);
+}
+@Test
+public void testAssertEqualsFalseWithMessage() {
+ServiceObject newServiceObject = new ServiceObject();
+junitAssertEqualsServiceSample.postProcessing(serviceObject);
+assertEquals("Not the Same Object",
+serviceObject,serviceObject);
+}
+
+}
