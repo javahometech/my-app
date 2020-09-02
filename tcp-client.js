01322381edc09ed9066e629498750271077d15ea
penglc <penglc@users.noreply.github.com>
2020-09-02 09:16:34 +0800

    Create tcp-client.js

---
+++ tcp-client.js
@@ -0,0 +1,19 @@
+var net = require('net') ;
+const stringRandom = require('string-random');
+
+var arguments = process.argv.splice(2);
+var IP = arguments.toString();
+if (IP == '') {
+    console.log("Format: node tcp-client.js IP")
+    process.exit();
+}
+var client = net.connect(8000, IP,function(){
+
+    client.name = 'clinet1';
+    //client.write(client.name + ' is up！\n');
+    client.write(client.name + ' is up！\n' + stringRandom(102400) + '\n~~~~~~~~~~~\n');
+    client.end(client.name + ' down！\n');
+    client.on("data", function(data) {
+        console.log(data.toString());
+    });
+});
