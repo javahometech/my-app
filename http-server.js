afdd357a58a2a08add6191b3da68201b0054eb5d
penglc <penglc@users.noreply.github.com>
2021-03-10 18:23:24 +0800

    Update http-server.js

--- http-server.js
+++ http-server.js
@@ -4,10 +4,6 @@ var app = express();
 var os = require('os');
 var fs = require("fs");
 
-
-const stringRandom = require('string-random');
-const str2 = stringRandom(10240);
-
 app.set('trust proxy', true)
 
 app.get('/', function (req, res) {
@@ -39,7 +35,7 @@ app.get('/check500', function (req, res) {
   res.send("501");
 })
 
-var server = app.listen(80, '0.0.0.0', function () {
+var server = app.listen(8080, '0.0.0.0', function () {
 
   var host = server.address().address;
   var port = server.address().port;
