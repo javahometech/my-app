2061b92062741f219782f80dc2ee86852098f94d
Mohan Reddy Bandarupalle <mohanreddybandarupalle@Mohans-MBP.home>
2021-06-04 19:03:58 -0400

    updated personal.sh

--- personal.sh
+++ personal.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 cd /Users/mohanreddybandarupalle/Personal/my-app
-if [[ $(git submodule deinit -f Terraform-2019jun) ]]
+if [[ $(git pull ]]
 then 
-rm -rf .git/modules/Terraform-2019jun
-git rm -f Terraform-2019jun
-git commit -a -m 'Deleted privacy-commom submodule  system date'
 git push
-echo "executed success"
+echo "push executed success"
 else
-echo "submodule not deleted"
+echo "push not deleted"
 fi
