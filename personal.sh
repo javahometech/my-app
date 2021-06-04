1d361e6df4b35e5a049626ba8e72a363fe61eb80
Mohan Reddy Bandarupalle <mohanreddybandarupalle@Mohans-MBP.home>
2021-06-04 14:13:00 -0400

    Deleted privacy-commom submodule  system date

--- personal.sh
+++ personal.sh
@@ -2,8 +2,8 @@
 cd /Users/mohanreddybandarupalle/Personal/my-app
 if [[ $(git submodule deinit -f Terraform-2019jun) ]]
 then 
-rm -rf .git/modules/Terraform-2019juns
-git rm -f privacy-commons
+rm -rf .git/modules/Terraform-2019jun
+git rm -f Terraform-2019jun
 git commit -a -m 'Deleted privacy-commom submodule  system date'
 git push
 echo "executed success"
