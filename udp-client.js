0d68de462653cd2c98815f9f57d896f3a6cc8983
penglc <penglc@users.noreply.github.com>
2021-11-02 23:25:57 +0800

    Update udp-client.js

--- udp-client.js
+++ udp-client.js
@@ -7,13 +7,20 @@ var client = udp.createSocket('udp4');
 //buffer msg
 var data = Buffer.from('siddheshrane');
 
+var arguments = process.argv.splice(2);
+var IP = arguments.toString();
+if (IP == '') {
+    console.log("Format: node udp-client.js IP")
+    process.exit();
+}
+
 client.on('message',function(msg,info){
   console.log('Data received from server : ' + msg.toString());
   console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
 });
 
 //sending msg
-client.send(data,2222,'localhost',function(error){
+client.send(data,2222,IP,function(error){
   if(error){
     client.close();
   }else{
@@ -25,7 +32,7 @@ var data1 = Buffer.from('hello');
 var data2 = Buffer.from('world');
 
 //sending multiple msg
-client.send([data1,data2],2222,'localhost',function(error){
+client.send([data1,data2],2222,IP,function(error){
   if(error){
     client.close();
   }else{
