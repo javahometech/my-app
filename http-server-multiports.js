85fd8ebf174359b96b295a9de34421d4c773423a
penglc <penglc@users.noreply.github.com>
2021-11-18 22:06:16 +0800

    Update http-server-multiports.js

--- http-server-multiports.js
+++ http-server-multiports.js
@@ -4,7 +4,8 @@ var servers = [];
 var s;
 
 function reqHandler(req, res) {
-  var response = 'Client IP:' + req.socket.remoteAddress + '\n';
+  var clientIP = getClientIP(req);
+  var response = 'Client IP:' + clientIP + '\n';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(response);
@@ -19,4 +20,16 @@ for (let port = 8000; port < 8050; port++) {
   s = http.createServer(reqHandler);
   s.listen(port);
   servers.push(s);
+}
+
+function getClientIP(req) {
+  //var strAry = req.connection.remoteAddress.split(':');
+  //return strAry[strAry.length -1];
+  //return req.connection.remoteAddress;
+  console.log("x-forwarded-for %s", req.headers['x-forwarded-for']);
+  console.log("Remote IP %s", req.connection.remoteAddress);
+  console.log("IPs %s", req.ips);
+
+  return (req.headers['x-forwarded-for'] || '').split(',')[0]
+        || req.connection.remoteAddress;
 }
